5856c46eee3bc2cc36356f5868ce0367ae80bc90
Josego525 <93600613+Josego525@users.noreply.github.com>
2025-03-26 18:00:30 -0500

    .

--- src/app/components/molecules/datos-contratista/datos-contratista.component.ts
+++ src/app/components/molecules/datos-contratista/datos-contratista.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { SubtitleComponent } from '../../atoms/subtitle/subtitle.component';
 import { TextInputComponent } from '../../atoms/text-input/text-input.component';
 import { SelectInputComponent } from '../../atoms/select-input/select-input.component';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TIPODOCUMENTO } from '../../../shared/interfaces/typo_documentos';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-datos-contratista',
@@ -16,14 +17,18 @@ import { TIPODOCUMENTO } from '../../../shared/interfaces/typo_documentos';
   templateUrl: './datos-contratista.component.html',
   styleUrl: './datos-contratista.component.css'
 })
-export class DatosContratistaComponent {
+export class DatosContratistaComponent implements OnInit, OnDestroy {
   @Input() form: FormGroup | undefined;
   @Input() lists: any = {};
 
   addressRegexp = /^[a-zA-Z0-9\s]+$/;
+  subs: Subscription[] = [];
+  documentNumberInputType: string = 'number';
 
   ngOnInit() {
     this.form?.controls['economic_activity'].disable();
+    this.subscribeForm();
+    this.onChageDocumentType();
   }
 
   getControl(controlName: string): FormControl {
@@ -43,5 +48,22 @@ export class DatosContratistaComponent {
 
   showPepff() {
     return this.form?.get('document_type_id')?.value == TIPODOCUMENTO.CE || this.form?.get('document_type_id')?.value == TIPODOCUMENTO.PASAPORTE;
+  }
+
+  subscribeForm() {
+    this.form?.get('document_type_id')?.valueChanges.subscribe(() => this.onChageDocumentType());
+  }
+
+  onChageDocumentType() {
+    if ([TIPODOCUMENTO.CE, TIPODOCUMENTO.PASAPORTE].includes(Number(this.form?.get('document_type_id')?.value))) {
+      this.documentNumberInputType = 'text';
+    } else {
+      this.documentNumberInputType = 'number';
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
   }
 }
